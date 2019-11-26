5c4ddcfb0e28f3785ce320c54f8213c4cf63f7ea
virsantillan1 <57966647+virsantillan1@users.noreply.github.com>
2019-11-25 23:14:28 -0300

    Add files via upload

---
+++ css/header.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/estilosH.css">
+		<title></title>
+	</head>
+	<body>
+		<div class="container">
+        <!-- Encabezado -->
+      <header>
+				<nav class="main-nav">
+					<a href="index.html"><img class="logo" src="img/gfpeque.png" alt="logo craftsy"></a>
+          <img class="icono" src="img/cart.png">
+          <img class="icono_2" src="img/cora.png">
+
+					<form class="menu">
+            <input type="text" name="buscador" value="" placeholder="Buscar">
+              <button>Buscar</button>
+          </form>
+
+					<ul class="acceso">
+						<li><a href="login.php" target="_blank">INICIAR SESIÓN</a></li>
+						<li><a href="register.php" target="_blank">REGISTRARME</a></li>
+					</ul>
+				</nav>
+			</header>
+    		</div>
+
+	</body>
+</html>
